23dcf68caf6dbdea0d7925f6b8c0f64ca45c5d4d
Mineeva Daria <kitysh@mail.ru>
2019-03-25 16:43:18 +0300

    remove unused code

--- src/test/java/ru/tinkoff/daria/selenium/test/TinkoffTariffPageTest.java
+++ src/test/java/ru/tinkoff/daria/selenium/test/TinkoffTariffPageTest.java
@@ -7,16 +7,6 @@ import org.openqa.selenium.*;
 import java.util.concurrent.TimeUnit;
 
 public class TinkoffTariffPageTest extends BaseRunner {
-    /*private WebDriver driver;
-    private final String baseUrl = "https://www.tinkoff.ru/mobile-operator/tariffs/";
-    private final String browserName = System.getProperty("browser") == null ? "chrome" : System.getProperty("browser");*/
-
- /*   @Before
-    public void setUp()  {
-        driver = BrowserFactory.buildDriver(browserName);
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-    }*/
 
     @Test
     public void testEmptyFieldsTestCase() {
@@ -36,7 +26,7 @@ public class TinkoffTariffPageTest extends BaseRunner {
 
         driver.findElement(By.name("fio")).click();
         driver.findElement(By.name("fio")).clear();
-        driver.findElement(By.name("fio")).sendKeys("ывапывап");
+        driver.findElement(By.name("fio")).sendKeys("нкгнукункг");
 
         driver.findElement(By.name("phone_mobile")).click();
         driver.findElement(By.name("phone_mobile")).clear();
@@ -44,7 +34,7 @@ public class TinkoffTariffPageTest extends BaseRunner {
 
         driver.findElement(By.name("email")).click();
         driver.findElement(By.name("email")).clear();
-        driver.findElement(By.name("email")).sendKeys("вапвавап");
+        driver.findElement(By.name("email")).sendKeys("шгншгншгннг");
         driver.findElement(By.name("email")).submit();
 
         assertEquals("Недостаточно информации. Введите фамилию, имя и отчество через пробел (Например: Иванов Иван Алексеевич)", driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Фамилия, имя и отчество'])[1]/following::div[3]")).getText());
@@ -52,8 +42,5 @@ public class TinkoffTariffPageTest extends BaseRunner {
         assertEquals("Введите корректный адрес эл. почты", driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Электронная почта'])[1]/following::div[3]")).getText());
     }
 
-    /*@After
-    public void tearDown()  {
-        driver.quit();
-    }*/
+
 }
